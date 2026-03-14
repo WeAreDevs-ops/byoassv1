0be2352facde72d848718313ec269b5a4321a74f
Niqqas <cleencuevas@gmail.com>
2026-03-14 20:58:49 +0800

    Add files via upload

--- birthdate-server.js
+++ birthdate-server.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const axios = require("axios");
 const { HttpsProxyAgent } = require("https-proxy-agent");
 
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public")); // Serve index.html from public folder
