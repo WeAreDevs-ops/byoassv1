495193e62be0920a0b396b2deee3522755b60afd
Niqqas <cleencuevas@gmail.com>
2026-03-14 23:11:46 +0800

    Update birthdate-server.js

--- birthdate-server.js
+++ birthdate-server.js
@@ -116,7 +116,7 @@ app.post("/api/change-birthdate", async (req, res) => {
         // STEP 1: Get CSRF Token
         logs.push("🔄 Step 1: Getting CSRF token...");
 
-        const csrf1 = await robloxRequest("https://users.roblox.com/v1/description", {
+        const csrf1 = await robloxRequest("https://auth.roblox.com/v2/logout", {
             method: "POST",
             headers: {
                 Cookie: roblosecurity,
